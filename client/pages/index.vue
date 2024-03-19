90ae80b490f59ca72882747ec747cd1755d7e8fe
Kent Joemar L. Escoto <kentescoto24@gmail.com>
2024-03-19 22:54:47 +0800

    update-login(UI)

---
+++ client/pages/index.vue
@@ -0,0 +1,25 @@
+<template lang="">
+  <div class="bg-[url('assets/img/bg/login-bg.jpg')] bg-center bg-cover bg-no-repeat h-screen w-full bg-black">
+    <div class="flex items-center lg:justify-start justify-center h-screen lg:p-24 p-0">
+      <form class="h-auto lg:w-1/2 w-3/4 bg-[#0C0F12] rounded shadow shadow-[#1C2E39] p-5 text-[#557479] tracking-wider">
+          <h1 class="cursor-default font-bold text-2xl mb-5 text-center">User Authentication</h1>
+          <div class="px-5 mb-5">   
+            <hr class="mb-5 border-[#557479]">
+            <label class="text-base">Username</label>
+            <input type="text" class="w-full bg-[#96a9ac] focus:bg-[#dbe2e3] py-2 px-3 rounded border border-black transition-all duration-300 outline-none focus:border-[#557479] text-black mb-5 mt-1">
+            <label class="text-base">Password</label>
+            <input type="password" class="w-full bg-[#96a9ac] focus:bg-[#dbe2e3] py-2 px-3 rounded border border-black transition-all duration-300 outline-none focus:border-[#557479] text-black mb-2 mt-1">
+            <p class="text-xs mb-3 text-end"><nuxt-link to="/">Forgot password?</nuxt-link></p>
+            <nuxt-link to="verified/main-page">
+              <button type="submit" class="w-full text-center bg-[#67171D] py-2 rounded mt-2 text-white tracking-wider opacity-90 hover:opacity-100 font-semibold">Login</button>
+            </nuxt-link>
+          </div>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
---
+++ client/pages/verified/main-page.vue
@@ -0,0 +1,62 @@
+<template lang="">
+  <div class="h-screen w-full bg-[#0E1213] p-3 tracking-wide">
+
+    <nav class="h-14 w-full bg-[#1C242B] rounded flex items-center p-3 justify-between shadow-lg">
+      <!-- <p class="text-black font-semibold">Main Page</p> -->
+      <div class="flex gap-4 text-[#a7b7ba]">
+        <p class="">Check Log</p>
+        <p class="">History</p>
+        <p class="">Help</p>
+      </div>
+      <div class="flex justify-between items-center">
+        <div class="bg-[#E5EEC1] p-1 rounded shadow-lg cursor-pointer">
+          <img class="w-auto h-6" src="/assets/img/icons/notification.png" alt="notification"/>
+        </div>
+      </div>
+    </nav>
+
+    <div class="mt-2 h-5/6 h- w-full grid grid-cols-7 gap-2">
+      <div class="bg-[#898989] rounded p-1 col-span-6 justify-center flex items-center">
+        <p class="text-red-800">Camera not found</p>
+      </div>
+
+      <div class="bg-[#1C242B] p-3 w-full h-auto rounded">
+        <div class="bg-[#185552] p-4 rounded">
+          <p class="text-white mb-2">System</p>
+          <hr>
+          <div class="flex gap-3 mt-3 justify-center">
+            <div class="bg-[#E5EEC1] px-3 py-1 rounded shadow-lg cursor-pointer">
+              <img class="w-auto h-6 m-auto" src="/assets/img/icons/settings.png"/>
+              <p class="text-xs font-semibold">Settings</p>
+            </div>
+            <div class="bg-[#E5EEC1] px-4 py-1 rounded shadow-lg cursor-pointer">
+              <img class="w-auto h-6 m-auto" src="/assets/img/icons/profile.png"/>
+              <p class="text-xs font-semibold">Profile</p>
+            </div>
+          </div>
+        </div>
+        <div class="bg-[#185552] p-4 rounded mt-3">
+          <p class="text-white mb-2">Camera Control</p>
+          <hr>
+          <p class="text-white text-sm tracking-widest mt-2">Brightness</p>
+          <div class="w-full flex justify-between gap-2 mt-2">
+            
+            <div class="bg-[#557C83] rounded shadow-lg p-1">
+              <img class="w-5 h-full" src="/assets/img/icons/b-down.png" alt="down"/>
+            </div>
+            <input type="range" class="w-full h-1 bg-gray-300 rounded-md appearance-none outline-none opacity-70 transition-opacity duration-200 hover:opacity-100 m-auto" min="0" max="100" step="1">
+            <div class="bg-[#557C83] rounded shadow-lg p-1">
+              <img class="w-5 h-full" src="/assets/img/icons/b-up.png" alt="up"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+
+<script setup>
+
+</script>
